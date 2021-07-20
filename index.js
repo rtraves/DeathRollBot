aab1ccbe6f3737f4786194eb9ca56839dd1cf021
Richard Traves <prixec@gmail.com>
2021-07-20 02:45:12 +0000

    tweaks to readability

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
 const client  = new Discord.Client()
-const min = 1; // this only exists because I'm not sure if I want to allow ranges or not
 const prefix = "!";
 
 client.on("ready", () => {
@@ -13,11 +12,9 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 	
-  if (command === 'ping') { // just a test
-	message.channel.send('Pong.');
-  } else if (command === 'roll') {
+    if (command === 'roll') {
 		const amount = parseInt(args[0]); // get an int from args '!roll 100'
-    message.channel.send(getRandom(min, amount));
+    message.reply(' rolled a ' + getRandom(1, amount));
 
 		if (isNaN(amount)) {
 			return message.reply('that doesn\'t seem to be a valid number.');
